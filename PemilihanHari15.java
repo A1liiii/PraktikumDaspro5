33857816c712fe9e341d5b665bbeea6e7ea17922
A1liiii <mhmmdalimirza@gmail.com>
2024-09-27 07:54:32 +0700

    mengupdate kode program sistem pemilihan hari

--- PemilihanHari15.java
+++ PemilihanHari15.java
@@ -4,27 +4,18 @@ import java.util.Scanner;
 public class PemilihanHari15 {
     public static void main(String[] args) {
         Scanner day = new Scanner(System.in);
-        System.out.print("Masukkan nama hari (dalam bahasa Inggris): ");
-        String dayName = day.next().toLowerCase();
+        System.out.print("masukan angka yang akan di convert ke dalam weekday atau weekend : ");
+        int daynumber = day.nextInt();
 
-        String dayType;
-        switch (dayName) {
-            case "monday":
-            case "tuesday":
-            case "wednesday":
-            case "thursday":
-            case "friday":
-                dayType = "Weekday";
-                break;
-            case "saturday":
-            case "sunday":
-                dayType = "Weekend";
-                break;
-            default:
-                dayType = "Invalid";
-                break;
+        if (daynumber >= 1 && daynumber <= 5) {
+            System.out.println("Angka"+daynumber+"dalam konversi hari adalah weekday");
+        } else if (daynumber == 6 || daynumber == 7) {
+            System.out.println("Angka"+daynumber+"dalam konversi hari adalah weekend");
+        } else {
+            System.out.println("Invalid Number");
         }
-        System.out.println("Hari " + dayName + " adalah " + dayType + ".");
+
+        day.close();
 
     }
 }
